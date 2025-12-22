6eaa37c97094985c1830b091671921356e851ba3
Karamath Ali <karamatali1472@gmail.com>
2025-12-23 04:24:15 +0530

    Epaper CMS Day 1 complete

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=epaper_cms',
+    'username' => 'epaper_user',
+    'password' => 'epaper123',
+    'charset' => 'utf8',
+];
